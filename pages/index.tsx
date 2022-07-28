a738a92ab3263a8b91a3e852d60acc3879fcf090
ehck ehck <throwawayehcksdee@gmail.com>
2022-07-28 10:46:19 -0400

    removed some comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from 'next/types'
 import Scene from '../components/scene'
 import UI from '../components/ui'
-// TODO: replace with UI ^^
-// TODO: have discord redirect open in new window
- 
+
 const Home: NextPage = () => {
 	return (
 		<>
